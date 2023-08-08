2d362d67cf84e671ea57b4e56f252f44c9371ea4
estherniyo <estherniyonkuru123@gmail.com>
2023-08-08 17:04:39 +0200

    explain constructor usage

--- classes/Classes.js
+++ classes/Classes.js
@@ -3,6 +3,8 @@
 class BankAccount {
     // You can't name a constructor anything you want 
     // You have to name it 'constructor' as it is what the processor knows
+
+    // A constructor is used to intialise the new created object it runs everyone an object is created
     constructor(name, age, title){ 
         this.name = name;
         this.age = age;
